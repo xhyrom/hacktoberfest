806a2aaecf165d5f4ce1cc33bc01014edda01521
Nitish Pal <nitishpal2013@gmail.com>
2022-10-16 23:24:22 +0530

    Temperature Converter in C++

--- code/C++/tempconv.cpp
+++ code/C++/tempconv.cpp
@@ -1,4 +1,4 @@
-// Program to convert temperature taken Celsius and transform into Fahrenheit.
+// Program to convert temperature units.
 
 
 
@@ -6,14 +6,61 @@
 
 using namespace std;
 
-float temp_Conv(float x){
+float Fahrenheit(float x){
     return (9/5.0)*x + 32.0;
 }
 
+float Celcius(float x){
+    return (5.0/9)*(x-32);
+}
+
+
 int main(){
-    float celsius;
-    cout<<"Enter the Temperature in Celsius : ";
-    cin>>celsius;
-    cout<<"Temperature in Fahrenheit is "<<temp_Conv(celsius)<<endl;
+    float input;
+    int type1,type2;
+    cout<<"Temperature converter"<<endl;
+    cout<<"Convert from (type, enter the respective) :\n";
+    cout<<"From Celsius -> 1 \n";
+    cout<<"From Fahrenheit -> 2 \n";
+    cout<<"From Kelvin -> 3 \n";
+    cin>>type1;
+    cout<<"Convert to (type, enter the respective) :\n";
+    cout<<"From Celsius -> 1 \n";
+    cout<<"From Fahrenheit -> 2 \n";
+    cout<<"From Kelvin -> 3 \n";
+    cin>>type2;
+    if(type1 == type2) 
+    {
+        cout<<"Wrong choice!!!"<<endl;
+        return 0;
+    }
+    cout<<"Enter the temperature : ";
+    cin>>input;
+    if (type1 == 1 && type2 == 2) // Celsius to Fahrenheit
+    {
+        cout<<Fahrenheit(input);
+    }
+    if (type1 == 1 && type2 == 3) // Celsius to Kelvin
+    {
+        cout<<(input + 273.16);
+    }
+    if (type1 == 2 && type2 == 1) // Fahrenheit to Celcius
+    {
+        cout<<Celcius(input);
+    }
+    if (type1 == 2 && type2 == 3) // Fahrenheit to Kelvin
+    {
+        cout<<Celcius(input) + 273.16;
+    }
+    if (type1 == 3 && type2 == 1) // Kelvin to Celcius
+    {
+        cout<<input - 273.16;
+    }
+    if (type1 == 3 && type2 == 2) // Kelvin to Fahrenheit
+    {
+        cout<<Celcius(input) - 273.16;
+    }
+    cout<<"\n";
     return 0;
+
 }
